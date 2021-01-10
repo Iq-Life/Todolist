26398e4266f71cd951088d794ec8d417e61d6b77
Iq-Life <iq23life@gmail.com>
2021-01-11 01:59:32 +0300

    Alarm Error

--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -13,19 +13,30 @@ export type PropsType = {
     removeTasks: (id: string) => void
     changeFilter: (value: FilterValueType) => void
     addTask: (title: string) => void
-    changeStatus : (id:string, isDone: boolean) => void
+    changeStatus: (id: string, isDone: boolean) => void
 }
 
 export function Todolist(props: PropsType) {
 
     let [title, setTitle] = useState("")
-
+    let [error, setError] = useState <string | null> ( null)
     const addTask = () => {
-        props.addTask(title)
-        setTitle("")
+        if (title.trim() !== "") {
+            props.addTask(title)
+            setTitle("")
+        }else{
+            setError("Title is required")
+        }
     }
-    const onChange = (e: ChangeEvent<HTMLInputElement>) => {setTitle(e.currentTarget.value)}
-    const onKeyPress = ({charCode}: KeyboardEvent<HTMLInputElement>) => {if (charCode === 13) {addTask()}}
+    const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setTitle(e.currentTarget.value)
+    }
+    const onKeyPress = ({charCode}: KeyboardEvent<HTMLInputElement>) => {
+        setError(null)
+        if (charCode === 13) {
+            addTask()
+        }
+    }
     const changeFilterAll = () => props.changeFilter("all")
     const changeFilterActive = () => props.changeFilter("active")
     const changeFilterCompleted = () => props.changeFilter("completed")
@@ -33,16 +44,19 @@ export function Todolist(props: PropsType) {
         <div className={s.Todo}>
             <h3>{props.title}</h3>
             <div>
-                <input className={s.input}
+                <input className={ error ? s.error : s.input }
                        value={title}
                        onChange={onChange}
                        onKeyPress={onKeyPress}/>
                 <button className={s.but_add} onClick={addTask}>add</button>
+                { error && <div className="error-message" >{error}</div>}
             </div>
             <ul className={s.ul}>
                 {props.tasks.map(t => {
-                    const onClickHandler = () => {props.removeTasks(t.id)}
-                    const onChangeChangeH = (e:ChangeEvent<HTMLInputElement>) => {
+                    const onClickHandler = () => {
+                        props.removeTasks(t.id)
+                    }
+                    const onChangeChangeH = (e: ChangeEvent<HTMLInputElement>) => {
                         let newIsDoneValue = e.currentTarget.checked;
                         props.changeStatus(t.id, newIsDoneValue)
                     }
