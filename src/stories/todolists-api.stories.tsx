f685dce547e7f02fdba551747fcb21a5a848d78b
IqLife <iq23@gmail.com>
2021-06-14 10:25:05 +0300

    Todolist axios.get,post,put,delete

--- src/stories/todolists-api.stories.tsx
+++ src/stories/todolists-api.stories.tsx
@@ -1,28 +1,57 @@
 import React, {useEffect, useState} from "react";
 import axios from 'axios'
 
-
 export default {
     title: 'API'
 }
 
-const settings= {
-    withCredentials: true
+const settings = {
+    withCredentials: true,
+    headers: {
+        "api-key": "d9baecdc-1ca4-440c-8d10-aee3256853c8"
+    }
 }
 
-export const GetTodolists = () =>{
+export const GetTodolists = () => {
     const [state, setState] = useState<any>(null)
     useEffect(() => {
-let promise = axios.get("https://social-network.samuraijs.com/api/1.1/todo-lists", settings)
-        promise.then((res) => {
-        setState(res.data)
-        })
-    },[])
+        axios.get("https://social-network.samuraijs.com/api/1.1/todo-lists", settings)
+            .then((res) => {
+                setState(res.data)
+            })
+    }, [])
     return <div> {JSON.stringify(state)}</div>
 }
 
 export const CreateTodolist = () => {
-
+    const [state, setState] = useState<any>(null)
+    useEffect(() => {
+        axios.post("https://social-network.samuraijs.com/api/1.1/todo-lists", {title: 'Dimych todolist'}, settings)
+            .then((res) => {
+                setState(res.data)
+            })
+    }, [])
+    return <div> {JSON.stringify(state)}</div>
 }
 
-export const DeleteTodolist = () => {}
+export const DeleteTodolist = () => {
+    const [state, setState] = useState<any>(null)
+    useEffect(() => {
+        axios.delete("https://social-network.samuraijs.com/api/1.1/todo-lists/{idTodolist}", settings)
+            .then((res) => {
+                setState(res.data)
+            })
+    }, [])
+    return <div> {JSON.stringify(state)}</div>
+}
+
+export const UpdateTodolist = () => {
+    const [state, setState] = useState<any>(null)
+    useEffect(() => {
+        axios.put("https://social-network.samuraijs.com/api/1.1/todo-lists/{idTodolist}", {title: 'Dimych todolist'}, settings)
+            .then((res) => {
+                setState(res.data)
+            })
+    }, [])
+    return <div> {JSON.stringify(state)}</div>
+}
