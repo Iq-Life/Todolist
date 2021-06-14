666c7961ba4f2b705f891afcd843679990e037ef
IqLife <iq23@gmail.com>
2021-06-14 09:40:26 +0300

    Todolist

---
+++ src/stories/EditableSpan.stories.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import {EditableSpan} from "../EditableSpan";
+import {ReduxStoreProviderDecorator} from "./ReduxStoreProviderDecorator.stories";
+import '@storybook/addon-console';
+export default {
+    title: "EditableSpan Component",
+    component: EditableSpan,
+    decorators: [ReduxStoreProviderDecorator]
+}
+
+//const changeTitleCallback = action("Value changed")
+
+export const EditableSpanBaseExample = () => {
+    return <>
+<EditableSpan title={"Start value"} changeTitle={()=> console.log("Value changed")} />
+    </>
+}
