d78110b9402090594c219010130de596db4f90ce
Pavel <73390739+Iq-Life@users.noreply.github.com>
2021-10-18 18:04:55 +0300

    Updates

---
+++ precache-manifest.30d8e21517911adb4ec8296b2b32a74d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78d499f13d26d27ece774dbab02bd5dd",
+    "url": "/Todolist/index.html"
+  },
+  {
+    "revision": "d96634730cb198f9cb44",
+    "url": "/Todolist/static/css/main.0dbcfb3b.chunk.css"
+  },
+  {
+    "revision": "e9f92d4a405407e1f077",
+    "url": "/Todolist/static/js/2.18e618bf.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Todolist/static/js/2.18e618bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cfe889c7a4876e811cd1",
+    "url": "/Todolist/static/js/3.38faf1ca.chunk.js"
+  },
+  {
+    "revision": "d96634730cb198f9cb44",
+    "url": "/Todolist/static/js/main.cb313ebf.chunk.js"
+  },
+  {
+    "revision": "c3b28fb0e9c0bbe272bd",
+    "url": "/Todolist/static/js/runtime-main.abfe533e.js"
+  }
+]);
